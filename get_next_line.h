317f9ae032ba849b2db3bd32a58953403821c618
Saba <egribassaba@gmail.com>
2025-12-23 20:01:47 +0100

    Initialize get_next_line header and add basic utility functions

---
+++ get_next_line.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: saba <saba@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/23 18:59:42 by saba              #+#    #+#             */
+/*   Updated: 2025/12/23 19:05:48 by saba             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+#include <unistd.h>
+#include <stdlib.h>
+
+# ifndef BUFFER_SIZE
+# define BUFFER_SIZE 42
+# endif
+
+
+#endif
+
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,7 +6,35 @@
 /*   By: saba <saba@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/21 18:11:16 by saba              #+#    #+#             */
-/*   Updated: 2025/12/21 18:11:22 by saba             ###   ########.fr       */
+/*   Updated: 2025/12/23 20:00:01 by saba             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "get_next_line.h"
+
+size_t gnl_strlen(char *s)
+{
+	size_t len;
+	
+	if (!s)
+		return(0);
+	len = 0;
+	while(s[len])
+		len++;
+	return(len);
+}
+int gnl_find_nl(const char *s, size_t stash_len)
+{
+	size_t i;
+	if (!s)
+		return (-1);
+	i = 0;
+	while(i < stash_len)
+	{
+		if(s[i] == '\n')
+			return (i);
+		i++;
+	}
+	return (-1);
+}
 
