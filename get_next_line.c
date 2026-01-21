4d90aeb18854f79fd54d699f14110da3ec6b2776
Saba <egribassaba@gmail.com>
2026-01-22 00:59:23 +0100

    started get_next_line.c file

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,28 @@
 /*   By: saba <saba@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/23 18:58:20 by saba              #+#    #+#             */
-/*   Updated: 2025/12/23 18:58:22 by saba             ###   ########.fr       */
+/*   Updated: 2025/12/24 18:13:34 by saba             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "get_next_line.h"
+
+char *get_next_line(int fd)
+{
+	static char *stash;
+	char		*buffer;
+	int			bytes_read;
+	
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (NULL);
+	
+	buffer = malloc(BUFFER_SIZE + 1);
+	if (buffer == NULL)
+		return (NULL);
+	bytes_read = 1;
+	
+	while(bytes_read > 0 && gnl_find_nl(stash, gnl_strlen(stash))== -1)
+		bytes_read = read(fd, buffer, BUFFER_SIZE);
+	
+	
+}
